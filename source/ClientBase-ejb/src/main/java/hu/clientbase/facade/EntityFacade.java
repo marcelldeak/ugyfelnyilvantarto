4042ce4494b7d7c5509975fa95287c86be053d50
Péter Torma <ptorma2011@gmail.com>
2016-05-13 22:26:53 +0200

    finish login nad reg page

---
+++ source/ClientBase-ejb/src/main/java/hu/clientbase/facade/EntityFacade.java
@@ -0,0 +1,46 @@
+package hu.clientbase.facade;
+
+import java.util.List;
+import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+
+@Singleton
+public class EntityFacade {
+    
+    @PersistenceContext(unitName = "crmPU")
+    protected EntityManager entityManager;
+
+    public EntityFacade() {
+        //default 
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    public <T> T create(T entity) {
+        entityManager.persist(entity);
+        return entity;
+    }
+
+    public <T> T update(T entity) {
+        return entityManager.merge(entity);
+    }
+
+    public <T> void delete(T entity) {
+        entityManager.remove(entity);
+    }
+
+    public <T> T find(Class<T> clazz, Object id) {
+        return entityManager.find(clazz, id);
+    }
+
+    public <T> List<T> findAll(Class<T> entityClass) {
+        CriteriaQuery cq = entityManager.getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        return entityManager.createQuery(cq).getResultList();
+    }
+
+}
