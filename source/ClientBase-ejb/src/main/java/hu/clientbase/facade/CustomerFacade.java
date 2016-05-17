6cbb4122d2bc3594e16615d04ad01d108109bf44
Csaba Kadlecsik <kadlecsik@outlook.com>
2016-05-17 15:51:10 +0200

    Add basic customer view

--- source/ClientBase-ejb/src/main/java/hu/clientbase/facade/CustomerFacade.java
+++ source/ClientBase-ejb/src/main/java/hu/clientbase/facade/CustomerFacade.java
@@ -1,19 +1,22 @@
 package hu.clientbase.facade;
 
 import hu.clientbase.entity.Customer;
-import hu.clientbase.entity.User;
+
+import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Stateless
 @LocalBean
-public class UserFacade {
+public class CustomerFacade {
 
-    @PersistenceContext
+    @PersistenceContext(name = "crmPU")
     private EntityManager em;
 
-
-
+    public List<Customer> getAllCustomers() {
+        return em.createQuery("SELECT c FROM Customer c", Customer.class).getResultList();
+    }
 }
---
+++ source/ClientBase-web/src/main/java/hu/clientbase/bean/mv/CustomerView.java
@@ -0,0 +1,44 @@
+package hu.clientbase.bean.mv;
+
+import hu.clientbase.dto.BasicCustomerDTO;
+import hu.clientbase.service.mv.CustomerModel;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named("customerView")
+@ViewScoped
+public class CustomerView implements Serializable {
+
+    private BasicCustomerDTO selectedCustomer;
+
+    private List<BasicCustomerDTO> customers;
+
+    @Inject
+    private CustomerModel model;
+
+    @PostConstruct
+    private void init() {
+        customers = model.getAllCustomers();
+    }
+
+    public BasicCustomerDTO getSelectedCustomer() {
+        return selectedCustomer;
+    }
+
+    public void setSelectedCustomer(BasicCustomerDTO selectedCustomer) {
+        this.selectedCustomer = selectedCustomer;
+    }
+
+    public List<BasicCustomerDTO> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<BasicCustomerDTO> customers) {
+        this.customers = customers;
+    }
+
+}
