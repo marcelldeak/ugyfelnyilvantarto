bcd14ea9ffb7f773a378437a3b55b7e179e0e57a
Csaba Kadlecsik <kadlecsik@outlook.com>
2016-05-18 10:43:37 +0200

    Fix HTML, CSS

--- source/ClientBase-web/src/main/java/hu/clientbase/bean/NavigationBean.java
+++ source/ClientBase-web/src/main/java/hu/clientbase/bean/NavigationBean.java
@@ -4,17 +4,18 @@ import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import org.omnifaces.util.Ajax;
 
 @Named("navigation")
 @ViewScoped
-public class NavigationBean implements Serializable{
-    
+public class NavigationBean implements Serializable {
+
     private String page;
-    
+
     @PostConstruct
-    public void init()
-    {
+    public void init() {
         page = "artifacts/home";
+        this.update();
     }
 
     public String getPage() {
@@ -23,6 +24,21 @@ public class NavigationBean implements Serializable{
 
     public void setPage(String page) {
         this.page = page;
+        this.update();
+    }
+
+    public String getMenuClass(String menuItem) {
+
+        if (page.equals(menuItem)) {
+            return "current";
+        } else {
+            return "";
+        }
+    }
+    
+    public void update()
+    {
+        Ajax.update("menu_list","a_page_content");
     }
 
 }
