013cd56086530c30652300ed9f7b0562b9c188a2
Csaba Kadlecsik <kadlecsik@outlook.com>
2016-05-20 15:14:16 +0200

    remove ajax navigation

--- source/ClientBase-web/src/main/java/hu/clientbase/bean/NavigationBean.java
+++ source/ClientBase-web/src/main/java/hu/clientbase/bean/NavigationBean.java
@@ -1,44 +1,26 @@
 package hu.clientbase.bean;
 
 import java.io.Serializable;
-import javax.annotation.PostConstruct;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import org.omnifaces.util.Ajax;
+import javax.servlet.http.HttpServletRequest;
 
 @Named("navigation")
 @ViewScoped
 public class NavigationBean implements Serializable {
 
-    private String page;
-
-    @PostConstruct
-    public void init() {
-        page = "artifacts/home";
-        this.update();
-    }
-
-    public String getPage() {
-        return page;
-    }
-
-    public void setPage(String page) {
-        this.page = page;
-        this.update();
-    }
-
     public String getMenuClass(String menuItem) {
 
-        if (page.equals(menuItem)) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        String uri[] = ((HttpServletRequest) context.getExternalContext().getRequest()).getRequestURI().split("/");
+        if (uri[uri.length - 1].startsWith(menuItem)) {
             return "current";
         } else {
             return "";
         }
-    }
-    
-    public void update()
-    {
-        Ajax.update("menu_list","a_page_content");
+
     }
 
 }
